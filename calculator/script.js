0cb353c7bdfa7bc1747ca4d8ddfd9a9b1775daca
Subrata <subrata@subrata.online>
2024-10-16 15:56:24 +0530

    update: calculator button added

--- calculator/script.js
+++ calculator/script.js
@@ -57,3 +57,52 @@ function claculate_result() {
     }
 
 }
+
+
+
+function set_active_input(id) {
+    if (id == 'input-box1') {
+        document.querySelector("#input-box1").classList.add("active_input");
+        document.querySelector("#input-box2").classList.remove("active_input");
+    } else if (id == "input-box2") {
+        document.querySelector("#input-box1").classList.remove("active_input");
+        document.querySelector("#input-box2").classList.add("active_input");
+    } else {
+        document.querySelector("#input-box1").classList.remove("active_input");
+        document.querySelector("#input-box2").classList.remove("active_input");
+    }
+
+}
+
+
+
+function append_to_input_box(number) {
+
+
+    // turnery operator 
+    // boolean ? state1 : state2
+    // true -> state1
+    // false -> state2
+
+
+    // let active_input = document.querySelectorAll(".input-box")[0].classList.contains("active_input") ?
+    //     document.getElementById("input-box1") : document.querySelectorAll(".input-box")[1].classList.contains("active_input") ?
+    //         document.getElementById("input-box2") : null
+
+
+    let active_input = null;
+
+    if (document.querySelector("#input-box1").classList.contains('active_input')) {
+        active_input = document.getElementById("input-box1");
+    }
+
+    else if (document.querySelector("#input-box2").classList.contains('active_input')) {
+        active_input = document.getElementById("input-box2");
+    }
+
+
+
+    // document.getElementById("input-box1").value = document.getElementById("input-box1").value + number
+
+    active_input.value += number
+}
